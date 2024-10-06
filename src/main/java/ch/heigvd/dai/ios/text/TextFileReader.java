f0c7ae2ad88da15f3b51e96a1a84e5dcb7810349
Lisa Gorgerat <lisa.gorgerat@heig-vd.ch>
2024-10-06 22:18:38 +0200

    ios with a few problem left

--- src/main/java/ch/heigvd/dai/ios/text/TextFileReader.java
+++ src/main/java/ch/heigvd/dai/ios/text/TextFileReader.java
@@ -2,6 +2,7 @@ package ch.heigvd.dai.ios.text;
 
 import ch.heigvd.dai.ios.Readable;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * A class that reads text files. This implementation reads the file byte per byte. It manages the
@@ -11,6 +12,25 @@ public class TextFileReader implements Readable {
 
   @Override
   public void read(String filename) {
-    throw new UnsupportedOperationException("Please remove this exception and implement this method.");
+    //buffer to read a file
+    Reader readerBuffer = null;
+
+    try {
+      readerBuffer = new FileReader(filename, StandardCharsets.UTF_8);
+
+      int b;
+      while ((b = readerBuffer.read()) != -1) {}
+
+    } catch (IOException e) {
+      System.err.println("Error: " + e.getMessage());
+    } finally {
+      if (readerBuffer != null) {
+        try {
+          readerBuffer.close();
+        } catch (IOException e) {
+          System.err.println("Error: " + e.getMessage());
+        }
+      }
+    }
   }
 }
--- src/main/java/ch/heigvd/dai/ios/text/TextFileWriter.java
+++ src/main/java/ch/heigvd/dai/ios/text/TextFileWriter.java
@@ -2,6 +2,7 @@ package ch.heigvd.dai.ios.text;
 
 import ch.heigvd.dai.ios.Writable;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * A class that writes text files. This implementation write the file byte per byte. It manages the
@@ -11,6 +12,26 @@ public class TextFileWriter implements Writable {
 
   @Override
   public void write(String filename, int sizeInBytes) {
-    throw new UnsupportedOperationException("Please remove this exception and implement this method.");
+
+    //class Writer is a buffer to write in a file
+    Writer writeBuffer = null;
+
+    try {
+      writeBuffer = new FileWriter(filename, StandardCharsets.UTF_8);
+      for (int i = 0; i < sizeInBytes; i++) {
+        writeBuffer.write('A');
+      }
+    } catch (IOException e) {
+      System.err.println("Error: " + e.getMessage());
+    } finally {
+      if (writeBuffer != null) {
+        try {
+          writeBuffer.close();
+        } catch (IOException e) {
+          System.err.println("Error: " + e.getMessage());
+        }
+      }
+    }
   }
+
 }
